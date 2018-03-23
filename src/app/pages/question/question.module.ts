ea1dc354c1f87cdf40a395ae0ea3386f874e4569
yuwzhang-company <potflesh@gmail.com>
2018-03-23 19:44:34 +0800

    : fix(pages): 增加页面的判断

--- src/app/pages/question/question.module.ts
+++ src/app/pages/question/question.module.ts
@@ -3,10 +3,27 @@ import { CommonModule } from '@angular/common';
 import { CommentComponent } from './comment/comment.component';
 import { EditorServiceComponent } from '../../shared/editor/editorService.component';
 import { QuestionComponent } from './question.component';
+import { MatExpansionModule, MatToolbarModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule, MatAutocompleteModule, MatButtonToggleModule, MatDividerModule, MatProgressBarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+     // material
+     BrowserAnimationsModule,
+     ReactiveFormsModule,
+     MatToolbarModule,
+     MatButtonModule,
+     MatCardModule,
+     MatFormFieldModule,
+     MatInputModule,
+     MatIconModule,
+     MatAutocompleteModule,
+     MatDividerModule,
+     MatButtonToggleModule,
+     MatProgressBarModule,
+     MatExpansionModule
   ],
   exports : [QuestionComponent],
   declarations: [CommentComponent, QuestionComponent],
