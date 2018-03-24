ddf4e6b7389691cd0bec3b0bb8491d5fe4ff0d1b
yuwzhang-company <potflesh@gmail.com>
2018-03-24 12:27:17 +0800

    : feat(index): add scorll card view

--- src/app/pages/index/index-feeds/index-feeds.component.ts
+++ src/app/pages/index/index-feeds/index-feeds.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
+declare var $: any;
 
 @Component({
   selector: 'app-pages-index-feeds',
@@ -16,11 +17,32 @@ export class IndexFeedsComponent implements OnInit {
     this.feeds.push(3);
     this.feeds.push(4);
     this.feeds.push(5);
+    this.test();
   }
 
   constructor() {
   }
-
+  
+  test() {
+    $.fn.fixedDiv = function(actCls){
+      var that = $(this),
+          offsetTop = that.offset().top,
+          scrollTop;
+      function fix(){
+          scrollTop = $(document).scrollTop();   
+          if (scrollTop > offsetTop ) {
+              that.addClass(actCls);
+              that.css('left','819.5px');
+          } else {
+              that.removeClass(actCls);
+          }
+      }
+      fix();
+      $(window).scroll(fix);
+  }
+  
+  $('#fix1').fixedDiv('fix-div');
+  }
 }
 
 
